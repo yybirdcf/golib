de00282ed0c123b9ed6e10cf3ca1cf7e47091dc3
chenhaipeng <1160616612@qq.com>
2019-05-27 00:07:38 +0800

    格式化

--- clog/clog.go
+++ clog/clog.go
@@ -1,10 +1,11 @@
 package clog
 
 import (
-	"go.uber.org/zap"
-	"github.com/natefinch/lumberjack"
-	"go.uber.org/zap/zapcore"
 	"sync"
+
+	"github.com/natefinch/lumberjack"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var once sync.Once
@@ -20,42 +21,42 @@ func InitLogger(logpath string, loglevel string) {
 // loglevel 日志级别
 func initLogger(logpath string, loglevel string) *zap.SugaredLogger {
 
-    hook := lumberjack.Logger{
-        Filename:   logpath, // 日志文件路径
-        MaxSize:    128,     // megabytes
-        MaxBackups: 30,      // 最多保留300个备份
-        MaxAge:     7,       // days
-        Compress:   true,    // 是否压缩 disabled by default
-    }
+	hook := lumberjack.Logger{
+		Filename:   logpath, // 日志文件路径
+		MaxSize:    128,     // megabytes
+		MaxBackups: 30,      // 最多保留30个备份
+		MaxAge:     7,       // days
+		Compress:   true,    // 是否压缩 disabled by default
+	}
 
-    w := zapcore.AddSync(&hook)
+	w := zapcore.AddSync(&hook)
 
-    // 设置日志级别,debug可以打印出info,debug,warn；info级别可以打印warn，info；warn只能打印warn
-    // debug->info->warn->error
-    var level zapcore.Level
-    switch loglevel {
-    case "debug":
-        level = zap.DebugLevel
-    case "info":
-        level = zap.InfoLevel
-    case "error":
-        level = zap.ErrorLevel
-    default:
-        level = zap.InfoLevel
-    }
-    encoderConfig := zap.NewProductionEncoderConfig()
-    // 时间格式
-    encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-    core := zapcore.NewCore(
-        zapcore.NewConsoleEncoder(encoderConfig),
-        w,
-        level,
-    )
+	// 设置日志级别,debug可以打印出info,debug,warn；info级别可以打印warn，info；warn只能打印warn
+	// debug->info->warn->error
+	var level zapcore.Level
+	switch loglevel {
+	case "debug":
+		level = zap.DebugLevel
+	case "warn":
+		level = zap.WarnLevel
+	case "error":
+		level = zap.ErrorLevel
+	default:
+		level = zap.InfoLevel
+	}
+	encoderConfig := zap.NewProductionEncoderConfig()
+	// 时间格式
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encoderConfig),
+		w,
+		level,
+	)
 
-    logger := zap.New(core).Sugar()
-    logger.Info("DefaultLogger init success")
+	logger := zap.New(core).Sugar()
+	logger.Info("DefaultLogger init success")
 
-    return logger
+	return logger
 }
 
 func Info(args ...interface{}) {
